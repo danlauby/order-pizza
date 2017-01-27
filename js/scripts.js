c4fcb857af3f1b0e358fe51e6fbe791eafc38dbf
danlauby <dmlauby@gmail.com>
2017-01-27 12:03:50 -0800

    customerPizza Object is reading inputs, running the prototype and returning desired properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,50 @@
 // Business Logic
 function Pizza(size) {
-  this.size = size;
-  this.toppings = [];
+  this.pizzaSize = size;
   this.price = 0;
+  this.toppings = [];
 }
 
-function Order(customerName, orderNumber) {
-  this.customerName = customerName;
-  this.orderNumber = orderNumber;
-}
+// function Order(customerName, orderNumber) {
+//   this.customerName = customerName;
+//   this.orderNumber = orderNumber;
+// }
 
 Pizza.prototype.cost = function() {
-  if (this.size === 'small') {
+  if (this.pizzaSize === 'small') {
     this.price = 6;
-    console.log(newPizza.price);
-  } else if (this.size === 'medium') {
-    this.price = 9;
-    console.log(newPizza.price);
-  } else if (this.size === 'large') {
-    this.price = 12;
-    console.log(newPizza.price);
+  } else if (this.pizzaSize === 'medium') {
+    this.price += 9;
+  } else if (this.pizzaSize === 'large') {
+    this.price += 12;
   }
-  for (i=0; i < this.toppings.length; i++) {
+  for (var i=0; i < this.toppings.length; i++) {
     this.price += 1;
-    console.log(this);
   }
 }
-
+// function displayOrder() {
+//   var html;
+//   $('.show-order').append(
+//     html += '<ul>';
+//     customerPizza.toppings.forEach(function(topping) {
+//       html += '<li>' + topping + '</li>';
+//     });
+//     html += '</ul>';
+//   );
+// }
 
 // User Interface Logic
-
-
-
 $(function() {
   $('form#customer-input').submit(function(e) {
-    var inputtedToppings =  $('input:checkbox[name="toppings"]:checked').each(function() {
-      customerPizza.toppings.push($(this).val());
-    });
+    var inputtedToppings =  $('input:checkbox[name="toppings"]:checked');
     var inputtedSize = $('#input-size').val();
     var customerPizza = new Pizza(inputtedSize);
-    console.log(customerPizza.size);
+    inputtedToppings.each(function() {
+      customerPizza.toppings.push($(this).val());
+    });
+    customerPizza.cost();
+    //displayOrder();
+    console.log(customerPizza);
     e.preventDefault();
   });
 
